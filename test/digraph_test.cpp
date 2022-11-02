f8192bd527d7af931997aa11e3017ee58c9c316f
Cridiba <cridiba99@gmail.com>
2022-11-02 13:55:51 +0100

    feat(#32pyber): list digraph building tests

--- test/digraph_test.cpp
+++ test/digraph_test.cpp
@@ -5,7 +5,7 @@ namespace digraph_test {
 
 using namespace graph;
 
-TEST_CASE("digraph is built correctly", "[building]") {
+TEST_CASE("digraph is built correctly", "[digraph]") {
   AdjacencyListDigraph g;
 
   SECTION("empty graph has no vertices or edges") {
@@ -61,6 +61,64 @@ TEST_CASE("digraph is built correctly", "[building]") {
     REQUIRE(g.edges().empty());
 
     REQUIRE_FALSE(g.remove_edge(e4));
+  }
+}
+
+TEST_CASE("digraph manages vertices and edges correctly", "[digraph]") {
+  AdjacencyListDigraph g;
+
+  SECTION("creating edges with new vertices adds them to graph") {
+    Vertex u = {100};
+    Vertex v = {200};
+    g.add_edge(u, v);
+    REQUIRE(g.edges().size() == 1);
+    REQUIRE(g.vertices().size() == 2);
+    for (Vertex vertex : g.vertices())
+      REQUIRE((vertex.id == u.id || vertex.id == v.id));
+  }
+
+  SECTION("removing a vertex the connected edges are deleted") {
+    Vertex u = g.add_vertex();
+    Vertex v = g.add_vertex();
+    Edge e1 = g.add_edge(u, v);
+    REQUIRE(g.edges().size() == 1);
+    REQUIRE_FALSE(g.edges().empty());
+    g.remove_vertex(u);
+    REQUIRE(g.edges().size() == 0);
+    REQUIRE(g.edges().empty());
+    REQUIRE(g.vertices().size() == 1);
+  }
+
+  SECTION("the outgoing edges are calculated correctly") {
+    Vertex u = g.add_vertex();
+    Vertex v = g.add_vertex();
+    Vertex z = g.add_vertex();
+    Edge e1 = g.add_edge(u, v);
+    Edge e2 = g.add_edge(v, v);
+    Edge e3 = g.add_edge(v, z);
+
+    REQUIRE_FALSE(g.out_edges(u).empty());
+    REQUIRE(g.out_edges(u).size() == 1);
+    REQUIRE_FALSE(g.out_edges(v).empty());
+    REQUIRE(g.out_edges(v).size() == 2);
+    REQUIRE(g.out_edges(z).empty());
+    REQUIRE(g.out_edges(z).size() == 0);
+    for (Edge edge : g.out_edges(v))
+      REQUIRE((edge.id == e2.id || edge.id == e3.id));
+  }
+
+  SECTION("the incoming edges are calculated correctly") {
+    Vertex u = g.add_vertex();
+    Vertex v = g.add_vertex();
+    Edge e1 = g.add_edge(u, v);
+    Edge e2 = g.add_edge(v, v);
+
+    REQUIRE(g.in_edges(u).empty());
+    REQUIRE(g.in_edges(u).size() == 0);
+    REQUIRE_FALSE(g.in_edges(v).empty());
+    REQUIRE(g.in_edges(v).size() == 2);
+    for (Edge edge : g.out_edges(v))
+      REQUIRE((edge.id == e1.id || edge.id == e2.id));
   }
 }
 
