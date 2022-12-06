6890fcc2e6cb1bd9344447fddf84628d92e6a765
Cridiba <cridiba99@gmail.com>
2022-12-07 00:21:02 +0100

    graphviz update

--- include/io/graphviz.hpp
+++ include/io/graphviz.hpp
@@ -1,19 +1,81 @@
+/**
+ * @file
+ * 
+ * @copyright Copyright © 2022 Graph++. All rights reserved.
+ *
+ * @license{<blockquote>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * </blockquote>}
+ *
+ * @author Matteo Cavaliere, Cristiano Di Bari, Michele Quaresmini, Andrea Cinelli
+ * @date December, 2022
+ * @version v1.0
+ *
+ */
+
 #include "base.hpp"
 #include "graph_concepts.hpp"
 #include <fstream>
+#include <functional>
 
 namespace graph::io::graphviz {
+
+/**
+ * @brief get the correct labels for dot language based on graph type
+ * @tparam GraphType type of graph object (directed/undirected)
+*/
 template <concepts::Orientable GraphType> struct GraphvizTraits {
-  static std::string name() { return "digraph"; }
-  static std::string delimiter() { return "->"; }
+  static std::string name();
+  static std::string delimiter();
 };
 
-template <> struct GraphvizTraits<UndirectedGraph> {
-  static std::string name() { return "graph"; }
-  static std::string delimiter() { return "--"; }
-};
-
+/**
+ * @brief writes a graph object into an output stream in graphviz DOT format
+ *        so that users can make use of graphviz to draw a picture with nice layout 
+ * @tparam G type of input graph
+ * @param[in] graph input graph object
+ * @param[out] out output stream
+*/
 template <concepts::Graph G> void serialize(std::ostream &out, const G &graph);
+
+/// @brief 
+/// @tparam G 
+/// @param out 
+/// @param graph 
+/// @param label_vertex 
+template <concepts::Graph G> void serialize(std::ostream &out, const G &graph, std::function<std::string(Vertex)> label_vertex);
+
+/// @brief 
+/// @tparam G 
+/// @param out 
+/// @param graph 
+/// @param label_vertex 
+/// @param label_edge 
+template <concepts::Graph G> void serialize(std::ostream &out, const G &graph, std::function<std::string(Vertex)> label_vertex, std::function<std::string(Edge)> label_edge);
+
+/**
+ * @brief interprets a graph described using the graphviz DOT language and builds 
+ *        a graph object that captures that description
+ * @tparam G type of input graph
+ * @param[in] in input stream
+*/
+template <concepts::Graph G> G deserialize(std::istream& in);
 
 } // namespace graph::io::graphviz
 
--- src/io/graphviz.i.hpp
+++ src/io/graphviz.i.hpp
@@ -1,19 +1,60 @@
 #include "base.hpp"
 #include "graph_concepts.hpp"
 #include <fstream>
+#include <functional>
+#include <string>
 
 namespace graph::io::graphviz {
-template <concepts::Graph G> void serialize(std::ostream &out, const G &graph) {
-  typedef GraphvizTraits<typename G::Tag> Traits;
 
-  out << Traits::name() << " "
-      << "{" << std::endl;
+template <concepts::Orientable GraphType> struct GraphvizTraits {
+  static std::string name() { return "digraph"; }
+  static std::string delimiter() { return "->"; }
+};
+
+template <> struct GraphvizTraits<UndirectedGraph> {
+  static std::string name() { return "graph"; }
+  static std::string delimiter() { return "--"; }
+};
+
+template <concepts::Graph G>
+void serialize(std::ostream &out, const G &graph,
+               std::function<std::string(Vertex)> label_vertex,
+               std::function<std::string(Edge)> label_edge) {
+
+  using Traits = GraphvizTraits<typename G::Tag>;
+
+  out << Traits::name() << " " << "{" << std::endl;
+
+  for (auto vertex : graph.vertices()) {
+    std::string vertex_label = label_vertex(vertex);
+    if (edge_label != "") {
+      out << vertex.id << " [label=\"" << vertex_label << "\"]" << ";" << std::endl;
+    }
+  }
 
   for (auto edge : graph.edges()) {
-    out << edge.u.id << Traits::delimiter() << edge.v.id << " ";
+    out << edge.u.id << Traits::delimiter() << edge.v.id;
+    std::string edge_label = label_edges(edge);
+    if (edge_label != "") {
+      out << " [label=\"" << edge_label << "\"]";
+    }
     out << ";" << std::endl;
   }
 
   out << "}" << std::endl;
 }
+
+template <concepts::Graph G>
+void serialize(std::ostream &out, const G &graph,
+               std::function<string(Vertex)> label_vertex) {
+  return serialize(out, graph, label_vertex, [](Edge) { return "" });
+}
+
+template <concepts::Graph G> void serialize(std::ostream &out, const G &graph) {
+  return serialize(
+      out, graph, 
+      [](Vertex v) { return "" },
+      [](Edge) { return "" });
+}
+
 } // namespace graph::io::graphviz
